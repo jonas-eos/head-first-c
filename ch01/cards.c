e152722035a7aba6383b50776ee12e94d2c8be3f
Jonas Evangelista <jonas.eos@outlook.com>
2019-06-21 22:08:21 -0300

    New functionality
    Now the cards programa run until the user inform X
    This check is happy on do...while statemant
    Added to a error message if users inform a invalid card name
    Added a feature that decrement or increment the card count with the
    following rule:
       *   If val >= 3 and <= 6 increases count
       *   If val == 10 decreases count

--- ch01/cards.c
+++ ch01/cards.c
@@ -8,29 +8,49 @@
 
 int main() {
   char card_name[3];
-  puts("Enter the card_name: ");
-  scanf("%2s", card_name);
   int val = 0;
-  switch (card_name[0]) {
-  case 'K':
-  case 'Q':
-  case 'J': {
-    val = 10;
-    break;
-  }
-  case 'A': {
-    val = 11;
-    break;
-  }
-  default: { val = atoi(card_name); }
-  }
+  int count = 0;
+  do {
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    switch (card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J': {
+      val = 10;
+      break;
+    } // Case K, Q and J
+    case 'A': {
+      val = 11;
+      break;
+    } // Case A
+    case 'X': {
 
-  /*Check if the value is 3 to 6 */
-  if ((val >= 3) && (val <= 6)) {
-    puts("Count has gone up");
-  } else if (val == 10) { // Otherwise check if the card was 10, J, Q or K
-    puts("Count has gone down");
-  }
-  printf("The card value is: %i\n", val);
+      /*
+       * Jump to next count on do...while loop, in this case will end the
+       * program.
+       */
+      continue;
+    } // Case X
+    default: { val = atoi(card_name); }
+
+      /*
+       * If card number is invalid, like 0 or less and 10 or high
+       * On this case, program will jump to next do...while loop
+       */
+      if ((val < 1) || (val > 10)) {
+        puts("I don't understand that value!");
+        continue;
+      } // if val < 1 and > 10
+    }   // case isn't J, Q, K, A and X value
+
+    /* Check if the value is 3 to 6 */
+    if ((val >= 3) && (val <= 6)) {
+      count++;
+    } else if (val == 10) { // Otherwise check if the card was 10, J, Q or K
+      count--;
+    }
+    printf("Current count: %i\n", count);
+  } while (card_name[0] != 'X');
   return 0;
 }
